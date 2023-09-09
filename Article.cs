0db59835bd9688ff87947114da2e04c13321fa4d
CurlySoldier <73424198+CurlySoldier@users.noreply.github.com>
2023-09-09 10:40:08 +0200

    feat: added article controller

---
+++ Article.cs
@@ -0,0 +1,10 @@
+﻿namespace WebsiteAPI;
+
+public class Article
+{
+    public string? Heading { get; set; }
+    public string? Author { get; set; }
+    public string? Contents { get; set; }
+    public DateTime? UploadedOn { get; set; }
+    
+}
---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ArticleController : ControllerBase
+{
+    private readonly ILogger<ArticleController> _logger;
+
+    public ArticleController(ILogger<ArticleController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetAvailableArticles")]
+    public async Task<IActionResult> Get()
+    {
+        var allArticles = new List<Article>();
+        await Task.Delay(5); // Simulate an asynchronous operation.
+        if (allArticles.Count == 0)
+        {
+            return NotFound();
+        }
+        return Ok(allArticles);
+    }
+}
